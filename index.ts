5af9230668e2f7643ffe83dd03b7617da72898a3
Us Farag <usama.farag@guidion.net>
2025-04-24 16:32:32 +0200

    chore: remove redundand index

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { SimpleCanvas, clearCanvas } from './src/SimpleCanvas/SimpleCanvas';
-export type { Point } from './src/interfaces/Point'; 
-export type { SimpleCanvasRef } from './src/SimpleCanvas/SimpleCanvas';
-
