80452259658526f650187b41db0e3725744fb233
Victor Sheyanov <1764195+vsheyanov@users.noreply.github.com>
2025-12-11 13:36:43 +0100

    Revert "fix(GDD-7196): fixed drawing Android bug"
    This reverts commit bfeb2acadb15a057538a00027bc790d752fa9dce.

--- src/SimpleCanvas.tsx
+++ src/SimpleCanvas.tsx
@@ -51,6 +51,7 @@ export const SimpleCanvas = ({
   const isDrawing = useRef(false);
 
   const canvasRef = useRef<View>(null);
+  const canvasOffset = useRef({ x: 0, y: 0 });
 
   const addPoint = useCallback((point: Point) => {
     pointsRef.current.push(point);
@@ -80,15 +81,22 @@ export const SimpleCanvas = ({
     onCanvasChange?.(false);
   }, [minPoints, onCanvasChange]);
 
+  const onLayout = useCallback(() => {
+    canvasRef.current?.measureInWindow((x, y) => {
+      canvasOffset.current = { x, y };
+    });
+  }, []);
+
   const panResponder = useMemo(
     () =>
       PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onMoveShouldSetPanResponder: () => true,
         onPanResponderGrant: (event) => {
+          const { pageX, pageY } = event.nativeEvent;
           const point = {
-            x: event.nativeEvent.locationX,
-            y: event.nativeEvent.locationY
+            x: pageX - canvasOffset.current.x,
+            y: pageY - canvasOffset.current.y
           };
           isDrawing.current = true;
           pointsRef.current = [point];
@@ -97,9 +105,10 @@ export const SimpleCanvas = ({
         },
         onPanResponderMove: (event) => {
           if (!isDrawing.current) return;
+          const { pageX, pageY } = event.nativeEvent;
           const point = {
-            x: event.nativeEvent.locationX,
-            y: event.nativeEvent.locationY
+            x: pageX - canvasOffset.current.x,
+            y: pageY - canvasOffset.current.y
           };
 
           if (addPoint(point)) {
@@ -149,6 +158,7 @@ export const SimpleCanvas = ({
     <View style={[styles.container, style]}>
       <View
         ref={canvasRef}
+        onLayout={onLayout}
         style={[styles.canvas, { backgroundColor }]}
         {...panResponder.panHandlers}
       >
